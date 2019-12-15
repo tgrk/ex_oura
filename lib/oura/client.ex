fd560d03f9a11419d7f7ad9edcb37636d213b58f
Martin Wiso <martin@wiso.cz>
2019-12-15 17:54:56 +0100

    Refactor client module

--- lib/oura/client.ex
+++ lib/oura/client.ex
@@ -1,11 +1,18 @@
-defmodule ExOuraring.Client do
+defmodule OuraCloudAPI.Client do
   @moduledoc """
   Documentation for Elixir client for Oura Ring API.
   """
-  @base_uri "https://cloud.ouraring.com"
 
-  @spec new(keyword) :: {:ok, OAuth2.Client.t()}
-  def new(opts) do
+  alias OuraCloudAPI.PersonalInfo
+
+  @base_uri "https://cloud.ouraring.com"
+  @token_uri "https://api.ouraring.com/oauth/token"
+
+  @type client :: OAuth2.Client.t()
+  @type error_result :: {:error, term()}
+
+  @spec new(keyword) :: {:ok, client()}
+  def new(opts \\ []) do
     client =
       OAuth2.Client.new(
         # default
@@ -19,22 +26,34 @@ defmodule ExOuraring.Client do
     {:ok, client}
   end
 
-  @spec authorize(OAuth2.Client.t()) :: binary
-  def authorize(client) do
-    OAuth2.Client.authorize_url!(client)
+  @spec authorize_url!(client()) :: binary()
+  def authorize_url!(client) do
+    @base_uri <> OAuth2.Client.authorize_url!(client)
   end
 
+  @spec get_token(client(), binary()) :: {:ok, client()} | error_result()
   def get_token(client, auth_code) do
     OAuth2.Client.get_token(client, code: auth_code)
   end
 
+  @spec get_user_info(client()) :: {:ok, PersonalInfo.t()} | error_result()
   def get_user_info(client) do
-    get(client, "/v1/user_info")
+    case get(client, "/v1/user_info") do
+      {:ok, body} ->
+        {:ok, struct(PersonalInfo, body)}
+
+      {:error, _reason} = error ->
+        error
+    end
   end
 
   def get_daily_summary(client, type, start_date, end_date)
       when type in [:sleep, :activity, :readiness] do
     get(client, "/v1/#{type}?start=#{format_date(start_date)}&end=#{format_date(end_date)}")
+  end
+
+  def get_daily_summary(_client, _type, _start_date, _end_Date) do
+    {:error, :unknown_type}
   end
 
   defp get(client, path) do
@@ -46,9 +65,11 @@ defmodule ExOuraring.Client do
   defp process_results({:ok, %OAuth2.Response{body: body}}) do
     {:ok, body}
   end
+
   defp process_results({:error, %OAuth2.Response{body: reason}}) do
     {:error, reason}
   end
+
   defp process_results({:error, %OAuth2.Error{reason: reason}}) do
     {:error, reason}
   end

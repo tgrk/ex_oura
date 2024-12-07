e0ca6eb06d17dd74816da3dfde5c7e709f21542d
Martin Wiso <martin@wiso.cz>
2024-12-07 17:59:24 +0100

    feat: add default API client

--- lib/client.ex
+++ lib/client.ex
@@ -1,10 +1,31 @@
 defmodule ExOura.Client do
   @moduledoc """
-  Default HTTP client
+  Default Oura client
   """
   use GenServer
 
+  @base_url "https://api.ouraring.com"
+
   # Client API
+
+  @doc """
+  Calls Oura Client modules
+  """
+  def call_api(mod, fun, args, opts) do
+    if client_ready?(opts) do
+      apply(mod, fun, args, opts)
+    else
+      {:error, :client_not_ready}
+    end
+  end
+
+  def date_range_args(start_date, end_date, next_token \\ nil) do
+    [
+      start_date: Calendar.strftime(start_date, "%Y-%m-%d"),
+      end_date: Calendar.strftime(end_date, "%Y-%m-%d"),
+      next_token: next_token
+    ]
+  end
 
   @doc """
   Starts the GenServer with an initial bearer auth token.
@@ -15,8 +36,8 @@ defmodule ExOura.Client do
 
   @doc """
   """
-  def request(method, args, opts \\ []) do
-    GenServer.call(__MODULE__, {:request, method, args, opts})
+  def request(operation) do
+    GenServer.call(__MODULE__, {:request, operation})
   end
 
   # Server Callbacks
@@ -27,31 +48,51 @@ defmodule ExOura.Client do
   end
 
   @impl true
-  def handle_call({:request, :get, url, args, opts}, _from, state) do
-    url
-    |> build_uri(args)
-    |> Req.get(opts)
-    |> handle_response()
+  def handle_call({:request, %{method: :get} = operation}, _from, state) do
+    reply =
+      operation
+      |> Map.get(:url)
+      |> Req.get(req_opts(operation, state))
+      |> handle_response(operation)
+
+    {:reply, reply, state}
   end
 
-  def auth(token) do
+  defp handle_response({:ok, %Req.Response{status: status, body: body}}, operation) do
+    case operation.response |> Enum.into(%{}) |> Map.get(status, nil) do
+      {response_module, _t} when status == 200 ->
+        {:ok, struct(response_module, body)}
+
+      {response_module, _t} ->
+        {:error, struct(response_module, body)}
+
+      :null ->
+        {:error, body}
+    end
+  end
+
+  defp handle_response({:error, _exception} = error, _operation), do: error
+
+  defp req_opts(operation, %{bearer_token: bearer_token} = _state) do
     [
-      auth: {:bearer, token}
+      base_url: @base_url,
+      params: Map.get(operation, :query, []),
+      auth: {:bearer, bearer_token},
+      decode_json: [keys: :atoms]
     ]
   end
 
-  defp handle_response({:ok, response} = r) do
-    r
-  end
+  defp apply(mod, fun, [] = _args, opts), do: apply(mod, fun, [opts])
+  defp apply(mod, fun, args, opts), do: apply(mod, fun, [args, opts])
 
-  defp handle_response({:error, exception} = e) do
-    e
-  end
-
-  defp build_uri(url, args) do
-    url
-    |> URI.parse()
-    |> Map.put(:query, URI.encode_query(args))
-    |> URI.to_string()
+  defp client_ready?(opts) do
+    if Keyword.has_key?(opts, :client) do
+      true
+    else
+      case Process.whereis(ExOura.Client) do
+        nil -> false
+        client_pid -> Process.alive?(client_pid)
+      end
+    end
   end
 end

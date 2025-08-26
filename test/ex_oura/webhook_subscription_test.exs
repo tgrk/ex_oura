46e6a373208e59f4803bcade19b994efd8832bce
Martin Wiso <martin@wiso.cz>
2025-08-26 10:43:08 +0200

    test: rewrite webhook tests to use ex_vcr

--- test/ex_oura/webhook_subscription_test.exs
+++ test/ex_oura/webhook_subscription_test.exs
@@ -1,7 +1,6 @@
 defmodule ExOura.WebhookSubscriptionTest do
-  use ExOura.Test.Support.Case, async: false
-
-  import Mock
+  use ExOura.Test.Support.Case, async: true
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Finch
 
   alias ExOura.Client.CreateWebhookSubscriptionRequest
   alias ExOura.Client.HTTPValidationError
@@ -9,20 +8,22 @@ defmodule ExOura.WebhookSubscriptionTest do
 
   describe "Webhook Subscription - list_webhook_subscription/1" do
     test "should return webhook subscriptions" do
-      with_mock Req, get: successful_response(:list_webhook_subscription, 200) do
+      use_cassette "list_webhook_subscription" do
         assert {:ok, [%WebhookSubscriptionModel{}]} =
                  ExOura.list_webhook_subscriptions()
       end
     end
 
     test "should return empty list when listing webhook subscriptions" do
-      assert {:ok, []} = ExOura.list_webhook_subscriptions()
+      use_cassette "list_webhook_subscription_empty" do
+        assert {:ok, []} = ExOura.list_webhook_subscriptions()
+      end
     end
   end
 
   describe "Webhook Subscription - create_webhook_subscription/2" do
     test "should successfully create a webhook subscription" do
-      with_mock Req, post: successful_response(:create_webhook_subscription, 201) do
+      use_cassette "create_webhook_subscription" do
         webhook = %CreateWebhookSubscriptionRequest{
           callback_url: "http://example.com",
           data_type: "tag",
@@ -36,58 +37,63 @@ defmodule ExOura.WebhookSubscriptionTest do
     end
 
     test "should fail when create a webhook subscription with invalid payload" do
-      webhook = %CreateWebhookSubscriptionRequest{
-        callback_url: "http://example.com",
-        data_type: "foo",
-        event_type: "create",
-        verification_token: "foobar"
-      }
+      use_cassette "create_webhook_subscription_invalid" do
+        webhook = %CreateWebhookSubscriptionRequest{
+          callback_url: "http://example.com",
+          data_type: "foo",
+          event_type: "create",
+          verification_token: "foobar"
+        }
 
-      assert {:error,
-              %HTTPValidationError{
-                detail: [
-                  %{
-                    type: "enum",
-                    msg:
-                      "Input should be 'tag', 'enhanced_tag', 'workout', 'session', 'sleep', 'daily_sleep', 'daily_readiness', 'daily_activity', 'daily_spo2', 'sleep_time', 'rest_mode_period', 'ring_configuration', 'daily_stress', 'daily_cycle_phases', 'activation_status', 'daily_cardiovascular_age', 'daily_resilience' or 'vo2_max'",
-                    loc: ["body", "data_type"]
-                  }
-                ]
-              }} = ExOura.create_webhook_subscription(webhook)
+        assert {:error,
+                %HTTPValidationError{
+                  detail: [
+                    %{
+                      type: "enum",
+                      msg: "Invalid data_type value",
+                      loc: ["body", "data_type"]
+                    }
+                  ]
+                }} = ExOura.create_webhook_subscription(webhook)
+      end
     end
   end
 
   describe "Webhook Subscription - delete_webhook_subscription/2" do
     test "should delete a webhook subscription" do
-      with_mock Req, delete: successful_response(:delete_webhook_subscription, 204) do
+      use_cassette "delete_webhook_subscription" do
         assert :ok =
                  ExOura.delete_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bfd")
       end
     end
 
     test "should fail when deleting a webhook subscription ID does not exists" do
-      assert {:error, %{detail: "No webhook id c534043e-ba54-4131-9dce-e40537823bff found."}} =
-               ExOura.delete_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bff")
+      use_cassette "delete_webhook_subscription_not_found" do
+        assert {:error, %{detail: "No webhook id c534043e-ba54-4131-9dce-e40537823bff found."}} =
+                 ExOura.delete_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bff")
+      end
     end
   end
 
   describe "Webhook Subscription - get_webhook_subscription/2" do
     test "should return a webhook subscription" do
-      with_mock Req, get: successful_response(:get_webhook_subscription, 200) do
+      use_cassette "get_webhook_subscription" do
         assert {:ok, %WebhookSubscriptionModel{}} =
                  ExOura.get_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bfd")
       end
     end
 
     test "should fail when getting a webhook subscription ID does not exists" do
-      assert {:error, %{detail: "Forbidden"}} =
-               ExOura.get_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bff")
+      use_cassette "get_webhook_subscription_forbidden" do
+        assert {:error, %{detail: "Forbidden"}} =
+                 ExOura.get_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bff")
+      end
     end
   end
 
   describe "Webhook Subscription - update_webhook_subscription/2" do
     test "should return an updated webhook subscription" do
-      with_mock Req, put: successful_response(:update_webhook_subscription, 200) do
+      use_cassette "update_webhook_subscription" do
         webhook = %CreateWebhookSubscriptionRequest{
           callback_url: "http://example.com",
           data_type: "foo",
@@ -104,87 +110,63 @@ defmodule ExOura.WebhookSubscriptionTest do
     end
 
     test "should fail when updating a webhook with an invalid payload" do
-      webhook = %CreateWebhookSubscriptionRequest{
-        callback_url: "http://example.com",
-        data_type: "tag",
-        event_type: "create"
-      }
+      use_cassette "update_webhook_subscription_invalid" do
+        webhook = %CreateWebhookSubscriptionRequest{
+          callback_url: "http://example.com",
+          data_type: "tag",
+          event_type: "create"
+        }
 
-      assert {
-               :error,
-               %HTTPValidationError{
-                 detail: [
-                   %{
-                     type: "string_type",
-                     msg: "Input should be a valid string",
-                     loc: ["body", "verification_token"]
-                   }
-                 ]
-               }
-             } =
-               ExOura.update_webhook_subscription(
-                 "c534043e-ba54-4131-9dce-e40537823bff",
-                 webhook
-               )
+        assert {
+                 :error,
+                 %HTTPValidationError{
+                   detail: [
+                     %{
+                       type: "string_type",
+                       msg: "verification_token is required",
+                       loc: ["body", "verification_token"]
+                     }
+                   ]
+                 }
+               } =
+                 ExOura.update_webhook_subscription(
+                   "c534043e-ba54-4131-9dce-e40537823bff",
+                   webhook
+                 )
+      end
     end
 
     test "should fail when updating a webhook subscription ID does not exists" do
-      webhook = %CreateWebhookSubscriptionRequest{
-        callback_url: "http://example.com",
-        data_type: "tag",
-        event_type: "create",
-        verification_token: "foobar"
-      }
+      use_cassette "update_webhook_subscription_not_found" do
+        webhook = %CreateWebhookSubscriptionRequest{
+          callback_url: "http://example.com",
+          data_type: "tag",
+          event_type: "create",
+          verification_token: "foobar"
+        }
 
-      assert {:error, %{detail: "Subscription with id c534043e-ba54-4131-9dce-e40537823bff not found."}} =
-               ExOura.update_webhook_subscription(
-                 "c534043e-ba54-4131-9dce-e40537823bff",
-                 webhook
-               )
+        assert {:error, %{detail: "Subscription with id c534043e-ba54-4131-9dce-e40537823bff not found."}} =
+                 ExOura.update_webhook_subscription(
+                   "c534043e-ba54-4131-9dce-e40537823bff",
+                   webhook
+                 )
+      end
     end
   end
 
   describe "Webhook Subscription - renew_webhook_subscription/2" do
     test "should return a renewed webhook subscription" do
-      with_mock Req, put: successful_response(:renew_webhook_subscription, 200) do
+      use_cassette "renew_webhook_subscription" do
         assert {:ok, %WebhookSubscriptionModel{}} =
                  ExOura.renew_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bfd")
       end
     end
 
     test "should fail when renewing a webhook subscription ID does not exists" do
-      assert {:error, %{detail: "Subscription with id c534043e-ba54-4131-9dce-e40537823bff not found."}} =
-               ExOura.renew_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bff")
-    end
-  end
-
-  defp successful_response(_operation, 204 = status) do
-    fn _url, opts ->
-      if valid_headers?(opts) do
-        {:ok, %Req.Response{status: status}}
-      else
-        {:error, :invalid_webhook_headers}
+      use_cassette "renew_webhook_subscription_not_found" do
+        assert {:error, %{detail: "Subscription with id c534043e-ba54-4131-9dce-e40537823bff not found."}} =
+                 ExOura.renew_webhook_subscription("c534043e-ba54-4131-9dce-e40537823bff")
       end
     end
-  end
-
-  defp successful_response(operation, status) do
-    fn _url, opts ->
-      if valid_headers?(opts) do
-        {:ok, %Req.Response{status: status, body: load_mock_fixture(operation)}}
-      else
-        {:error, :invalid_webhook_headers}
-      end
-    end
-  end
-
-  defp valid_headers?(opts) do
-    headers = opts |> Keyword.get(:headers) |> Map.new()
-    Map.has_key?(headers, "x-client-id") && Map.has_key?(headers, "x-client-secret")
-  end
-
-  defp load_mock_fixture(operation) do
-    path = Path.expand("fixture/mock/#{operation}.json", __DIR__)
-    path |> File.read!() |> Jason.decode!()
   end
 end

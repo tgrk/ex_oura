d1f5842559f5aeb8ed582bd73002d5a6605cfd08
Martin Wiso <martin@wiso.cz>
2026-04-17 09:18:15 +0200

    fix: harden upgrade decoder regression coverage

--- test/ex_oura/daily_heart_rate_test.exs
+++ test/ex_oura/daily_heart_rate_test.exs
@@ -28,5 +28,15 @@ defmodule ExOura.HeartRateTest do
       assert {:error, :invalid_start_datetime} =
                ExOura.multiple_heart_rate(~D[2024-11-11], ~U[2024-11-12 00:00:00Z])
     end
+
+    test "should reject invalid end datetime params for datetime endpoints" do
+      assert {:error, :invalid_end_datetime} =
+               ExOura.multiple_heart_rate(~U[2024-11-11 00:00:00Z], ~D[2024-11-12])
+    end
+
+    test "should reject inverted datetime ranges" do
+      assert {:error, :end_datetime_before_start_datetime} =
+               ExOura.multiple_heart_rate(~U[2024-11-12 00:00:00Z], ~N[2024-11-11 00:00:00])
+    end
   end
 end
